5e7f603d5fe3a965830b788d034b3870a95625c4
Lucas Scazzariello <lucas.scazza@gmail.com>
2021-02-22 19:23:17 -0300

    fix

--- database.js
+++ database.js
@@ -1,10 +1,25 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/rastro-db', {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// })
+//     .then(db => console.log('DB is connected'))
+//     .catch(err => console.log(err))
+
+
+
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/rastro-db', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-})
+mongoose.connect(`mongodb+srv://rastrouser:${process.env.DBPASS}@cluster-rastro.hbgco.mongodb.net/rastrodb?retryWrites=true&w=majority`, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err))
