b013b44f98160d414e684f3a7c57294d01796e13
Lucas Scazzariello <lucas.scazza@gmail.com>
2021-02-23 16:04:07 -0300

    borro codigo comentado

--- database.js
+++ database.js
@@ -1,16 +1,3 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://localhost/rastro-db', {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-// })
-//     .then(db => console.log('DB is connected'))
-//     .catch(err => console.log(err))
-
-
-
 require('dotenv').config()
 
 const mongoose = require('mongoose');
